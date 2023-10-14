1f9e9d724773faf3a4b3dc1fe25547bedcc36f93
Duc Vo <dvo4@seattleu.edu>
2023-10-13 19:18:54 -0700

    upgrade to dotnet 8.0

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace web.Pages;
+namespace desimone.Pages;
 
 public class IndexModel : PageModel
 {
